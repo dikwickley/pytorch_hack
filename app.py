fa5b414e029f385d5e90559c449f44f80d56fb59
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-08-23 15:14:13 +0530

    Coontact page added

--- app.py
+++ app.py
@@ -134,5 +134,9 @@ def logout():
 def about():
 	return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
