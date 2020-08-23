654c907eda5ba0d8227e8bc2a5fa5fe3c4d8c29d
Aniket Singh Rawat <sprx077@gmail.com>
2020-08-23 16:33:59 +0530

    integrated annotation

--- app.py
+++ app.py
@@ -116,14 +116,65 @@ def account():
 		email = session['user']
 		if 'forget' in session:
 			session.pop('forget', None)
-			session.pop('user', None)
 		data = mongo.db.user.find_one({"email" : email})
 		del data['_id']
 		print("DATA: ",data)
 		return render_template('account.html', data=json.dumps(data))
 	else:
 		return redirect(url_for('main'))
-           
+		
+
+@app.route('/annotation', method=['POST', 'GET'])
+def annotation():
+	if request.method == 'POST':
+		# check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        else:
+        	file.save(os.path.join('./static/upload/', file.filename))
+        	return render_template('annotation.html',res=json.dumps({'file': '../static/upload/'+file.filename, 'email': request.form['email']}))
+    else:
+    	return redirect('account')
+
+@app.route('/processData', methods=['POST','GET'])
+def processData():
+    if request.method == 'POST':
+
+        
+        
+        data = dict(request.form)
+        anno = {}
+        csv_data = [['filename','width','height','class','xmin','ymin','xmax','ymax']]
+        row = []
+
+        print('List start')
+
+        for x in range(0, int(len(data)/10)):
+            cell_list = []
+            cell_list.append(data['data['+str(x)+'][target][source]'].split('/')[-1]) #filename
+            cell_list.append(resolution('./static/upload/' + data['data['+str(x)+'][target][source]'].split('/')[-1])[1]) #width
+            cell_list.append(resolution('./static/upload/' + data['data['+str(x)+'][target][source]'].split('/')[-1])[0]) #height
+            cell_list.append(data['data['+str(x)+'][body][0][value]']) #class
+            cell_list.append(data['data['+str(x)+'][target][selector][value]'].split(':')[-1].split(',')[0]) #xmin
+            cell_list.append(data['data['+str(x)+'][target][selector][value]'].split(':')[-1].split(',')[1]) #ymin
+            cell_list.append(data['data['+str(x)+'][target][selector][value]'].split(':')[-1].split(',')[2]) #xmax
+            cell_list.append(data['data['+str(x)+'][target][selector][value]'].split(':')[-1].split(',')[3]) #ymax
+
+            csv_data.append(cell_list)
+    
+        email = session['user']     
+        with open('./static/user/'+email+'.csv', 'w+', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(csv_data)
+
+
 
 @app.route('/logout')
 def logout():
